1d8af78efe61b9c3c7f396fa166a2323367a1e58
marcomaret <marettimarco@outlook.it>
2021-02-19 12:33:15 +0100

    Sostituito campo data con campo path

--- oggetti.py
+++ oggetti.py
@@ -7,11 +7,11 @@ Created on Thu Feb 11 13:09:56 2021
 """
 
 class OggettoRicerca:
-    def __init__(self, idg, nome="", summary="", data="", rating=0):
+    def __init__(self, idg, nome="", summary="", path="", rating=0):
         self.id = idg
         self.nome = nome
         self.summary = summary
-        self.data = data
+        self.path = path
         self.rating = rating
     
     # setter eseguiti tramite assegnamento esterno
